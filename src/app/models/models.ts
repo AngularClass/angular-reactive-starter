f232865a1042a2809a70d4e84f6c62bfb2995ea1
gdi2290 <github@gdi2290.com>
2015-05-30 23:51:11 -0700

    chore(models/models): include greet injectables

--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -1,5 +1,7 @@
 import {counterInjectables} from './CounterModel';
+import {greetInjectables} from './GreetModel';
 
 export var modelInjectables = [
-  counterInjectables
+  counterInjectables,
+  greetInjectables
 ]
